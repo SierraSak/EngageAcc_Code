4e67da595b62b59a54d13fdb175efa99c1ce2bc9
Raytwo <raytwost@gmail.com>
2023-10-08 22:05:26 +0200

    Rewrite unitaccessorylist_is_exist_hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(lazy_cell, ptr_sub_ptr)]
+use engage::gamedata::{StructDataGeneric, StructData, StructDataStaticFields};
 use unity::prelude::*;
 
 #[unity::class("App", "AccessoryData")]
@@ -143,29 +144,28 @@ pub fn clear_UnitAccessoryList_hook(this: &mut UnitAccessoryList, method_info: O
     //}
 //}
 
-//#[unity::hook("App", "UnitAccessoryList", "IsExist")]
-//pub fn unitaccessorylist_is_exist_hook(this: &mut Il2CppObject<unit_accessory_list>, accessory: &mut Il2CppObject<app_accessorydata>, method_info: OptionalMethod,)-> bool
-//{
-    //if accessory == 0x0{
-    //return false;
-    //}
-    //else{
-        //let mut i = 0;
-        //let mut acc_index = 0;
-        //let mut acc_check;
-        //while i < this.unit_accessory_array.len()
-        //{
-            //acc_index = this.unit_accessory_array[i].index;
-            //acc_check = TryGet Accessory XML data from acc_index
-            //if acc_check == accessory{
-                //return true;
-            //}
-            //i += 1;
-        //}
-        //return false;
-//}
+#[unity::hook("App", "UnitAccessoryList", "IsExist")]
+pub fn unitaccessorylist_is_exist_hook(this: &mut UnitAccessoryList, accessory: Option<&mut AccessoryData>, method_info: OptionalMethod) -> bool
+{
+    // This is your old "if accessory == 0x0 {}". In the context of talking with C, Rust allows you to use Option<> on a pointer to signify that it could be null.
+    // That gives you plenty of fancy ways to check for null
+    accessory.is_some_and(|accessory| {
+        let structdata: &Il2CppClass = get_generic_class!(StructDataGeneric<AccessoryData>).unwrap();
+        let accessory_table = Il2CppObject::<StructData>::from_class(structdata).unwrap();
+        let accessories = accessory_table.get_class().get_static_fields::<StructDataStaticFields<AccessoryData>>().s_list.list.items;
 
-
+        // Looks for the AID of the provided accessory in the XML and return the index of the matching entry
+        this.unit_accessory_array
+            .iter() // Go through every entry in the array.
+            .any(|curr_acc| { // Confirms if any of the items in the array fulfills the condition.
+                // Grab the AccessoryData at that index in the XML if it's present, and if it is, compare the AIDs.
+                // Return false if the index is out of bounds OR the AIDs don't match
+                accessories.get(curr_acc.index as usize).is_some_and(|item| {
+                    item.aid.get_string().unwrap() == accessory.aid.get_string().unwrap()
+                })
+            })
+    })  
+}
 
 #[unity::hook("App", "UnitAccessoryList", ".ctor")]
 pub fn unitaccessorylist_ctor_hook(this: &mut UnitAccessoryList, method_info: OptionalMethod,)
@@ -186,7 +186,7 @@ pub fn onselectmenuitem_accessory_data_hook(this: &(), accessory_data: &mut Acce
 
 #[skyline::main(name = "TestProject")]
 pub fn main() {
-    skyline::install_hooks!(unitaccessorylist_ctor_hook, onbuild_accessory_data_hook, app_unitaccessorylist_getcount, clear_UnitAccessoryList_hook);
+    skyline::install_hooks!(unitaccessorylist_ctor_hook, onbuild_accessory_data_hook, app_unitaccessorylist_getcount, clear_UnitAccessoryList_hook, unitaccessorylist_is_exist_hook);
     skyline::patching::Patch::in_text(0x01f61c00).bytes(&[0x01, 0x02, 0x80, 0x52]).expect("Couldn’t patch that shit for some reasons");
     skyline::patching::Patch::in_text(0x027b5d70).bytes(&[0xDF, 0x3E, 0x00, 0x71]).expect("Couldn’t patch that shit for some reasons");
     skyline::patching::Patch::in_text(0x027b5d8c).bytes(&[0xDF, 0x42, 0x00, 0x71]).expect("Couldn’t patch that shit for some reasons");
